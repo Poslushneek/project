ca3ce3ff4fc19e64c00b5f43b1a901df43eb3c2a
Poslushneek <crossredgrey@gmail.com>
2023-07-20 17:56:24 +0500

    Initial commit

---
+++ task37/Program.cs
@@ -0,0 +1,19 @@
+﻿{
+    int[] array = { 1, 2, 3, 4, 5 };
+
+    Console.WriteLine("Исходный массив: {0}", GetArrayString(array));
+
+    int[] productArray = new int[array.Length / 2];
+
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        productArray[i] = array[i] * array[array.Length - 1 - i];
+    }
+
+    Console.WriteLine("Массив произведений пар чисел: {0}", GetArrayString(productArray));
+}
+
+static string GetArrayString(int[] array)
+{
+    return "[" + string.Join(" ", array) + "]";
+}
